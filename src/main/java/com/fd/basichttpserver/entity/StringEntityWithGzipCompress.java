a035e92c2a3706ad9a80ffb926d392814f539ae0
caoliuyi <caoliuyi@192.168.1.206>
2014-12-03 09:27:23 +0800

    commit

--- src/main/java/com/fd/basichttpserver/entity/StringEntityWithGzipCompress.java
+++ src/main/java/com/fd/basichttpserver/entity/StringEntityWithGzipCompress.java
@@ -1,6 +1,7 @@
 package com.fd.basichttpserver.entity;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.zip.GZIPOutputStream;
@@ -15,14 +16,30 @@ public class StringEntityWithGzipCompress extends StringEntity {
 			throws UnsupportedEncodingException {
 		super(string);
 	}
+
 	public StringEntityWithGzipCompress(String result, ContentType create) {
-		super(result,create);
+		super(result, create);
 	}
+
+	public long getContentLength() {
+		return -1;
+	}
+
+	public boolean isChunked() {
+		// force content chunking
+		return true;
+	}
+
+	public InputStream getContent() throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
 	public void writeTo(final OutputStream outstream) throws IOException {
 		Args.notNull(outstream, "Output stream");
-	    GZIPOutputStream gzip = new GZIPOutputStream(outstream);
+		GZIPOutputStream gzip = new GZIPOutputStream(outstream);
 		gzip.write(this.content);
-	    gzip.flush();
-
+		gzip.flush();
+		gzip.close();
+		outstream.flush();
 	}
 }
